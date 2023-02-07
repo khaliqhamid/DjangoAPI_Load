bb03f6d256e3c34a4ca57ee56734152b04ba80d9
Abdul Khaliq <khaliqhamid@hotmail.com>
2023-02-07 11:50:10 +0300

    data Table applied... Alhamolill Allah

--- market/views.py
+++ market/views.py
@@ -6,9 +6,11 @@ import json
 def index(request):
     # api_request= requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_96563a291e3c494ea7ec034295e639f9")
     api_request= requests.get("https://portal.gulfcable.com:8443/API/Survey/GetEmpSurveyResponse?empNo=02217&surveyYear=2022-01")
+   
     try:
-        api=json.loads(api_request.content)
+        api = json.loads(api_request.content)
     except Exception as e:
+        # api.raise_for_status()
         api="Error, data not loading"    
     
     return render(request, 'index.html', {'api':api})

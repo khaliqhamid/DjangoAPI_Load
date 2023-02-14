81f3f7fcd078c88285bc305256df697850f7b616
Abdul Khaliq <khaliqhamid@yahoo.com>
2023-02-14 13:50:39 +0300

    Alhamdolill Allah.......... image showing in survey form

--- market/urls.py
+++ market/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.index, name='index'),
     path('test/', views.test, name='test'),
+    path('surveylist/', views.survey, name='surveylist'),
     
 ]
--- market/views.py
+++ market/views.py
@@ -6,6 +6,8 @@ import json
 def index(request):
     # api_request= requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_96563a291e3c494ea7ec034295e639f9")
     api_request= requests.get("https://portal.gulfcable.com:8443/API/Survey/GetEmpSurveyResponse?empNo=02217&surveyYear=2022-01")
+    # api_request = requests.get("https://portal.gulfcable.com:8443/API/Survey/GetSurveyStatusV2?department=IT&departmentId=&status=Surveyed&question&questionId=0&chartQuestionLabel=&chartFor=&SurveyYear=2022-01")
+    
    
     try:
         api = json.loads(api_request.content)
@@ -18,3 +20,17 @@ def index(request):
 
 def test(request):
     return render(request, 'test.html')
+
+
+def survey(request):
+    # surveyApi_request = requests.get("https://portal.gulfcable.com:8443/API/Survey/GetSurveyStatusV2?department=IT&departmentId=&status=Surveyed&question&questionId=0&chartQuestionLabel=&chartFor=&SurveyYear=2022-01")
+    surveyApi_request = requests.get("https://portal.gulfcable.com:8443/API/Survey/GetSurveyStatusV2?department=NULL&departmentId=&status=Surveyed&question&questionId=0&chartQuestionLabel=&chartFor=&SurveyYear=2022-01")
+
+    try:
+        surveyApi = json.loads(surveyApi_request.content)
+
+    except Exception as e:
+        surveyApi =  "Error, data not loading"
+
+    return render(request, 'surveylist.html', {'surveyApi': surveyApi})
+    
